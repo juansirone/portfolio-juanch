0f0f44373ce558bffe2c0d195b3fea360f78a12d
Juan Chinchilla <juan.chinchilla10@gmail.com>
2026-03-05 09:06:13 -0600

    feat: workflow to upload with github actions

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,8 @@ import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://juansirone.github.io',
+  base: '/portfolio-juanch/',
   integrations: [tailwind()],
   i18n: {
     locales: ['en', 'es'],

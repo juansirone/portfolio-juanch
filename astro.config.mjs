58d3fe0518a8a70989e31cd731fc3ce7858d13b5
Juan Chinchilla <juan.chinchilla10@gmail.com>
2026-03-05 09:49:13 -0600

    fix: base url for public files

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,12 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 
+const isProd = process.env.NODE_ENV === 'production';
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://juansirone.github.io',
-  base: '/portfolio-juanch/',
+  base: isProd ? '/portfolio-juanch/' : '/',
   integrations: [tailwind()],
   i18n: {
     locales: ['en', 'es'],
